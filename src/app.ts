5c283da4104be2bcac4205c2d6927118a4048aac
Yale Araujo <yale850@gmail.com>
2025-02-14 14:48:04 -0300

    feat: server and app separeted to simplify tests

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
-import express, {Request, Response} from "express";
+import express from "express";
+import { usersRoutes } from "./http/controllers/users/routes";
+import { env } from "./env";
 
 export const app = express();
+
+app.use(express.json());
+app.use(usersRoutes);
+
+
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "./app";
 import { env } from "./env";
 
-app.listen(env.PORT, () => {
+
+app.listen(env.PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${env.PORT}`);
 });
