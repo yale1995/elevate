bc61764d0dcc753f9db68e5f65cdebbfae687d0c
Yale Araujo <yale850@gmail.com>
2025-02-18 23:32:42 -0300

    feat: new route setted: /users/authenticate

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 import { register } from './register'
+import { authenticate } from './authenticate'
 
 export const usersRoutes = Router()
 
 usersRoutes.post('/users/register', async (request, response) => {
   await register(request, response)
+})
+
+usersRoutes.post('/users/authenticate', async (request, response) => {
+  await authenticate(request, response)
 })
